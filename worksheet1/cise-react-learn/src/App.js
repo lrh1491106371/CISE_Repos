8804d33bff387c5867ef0563f7f480e375172b3b
lrh1491106371 <ruihaolin0209@gmail>
2025-09-09 15:35:55 +0800

    feat: Update App.js link to AUT website

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://aut.ac.nz" // Changed from reactjs.org
           target="_blank"
           rel="noopener noreferrer"
         >
